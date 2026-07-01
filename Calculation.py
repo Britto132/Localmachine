2491ba3cfd095e3e4fdeecba1fa40c8542801062
Britto132 <01fe23bca132@kletech.ac.in>
2026-07-01 11:25:27 +0530

    Practice2 repo

---
+++ Calculation.py
@@ -0,0 +1,6 @@
+a = 10
+b = 50
+print("The sum of two numbers is: ", a + b)
+print("The difference of two numbers is: ", a - b)
+print("The product of two numbers is: ", a * b)
+print("The division of two numbers is: ", a / b)
